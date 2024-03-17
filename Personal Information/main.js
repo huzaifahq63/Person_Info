75930cf576952031666af2e192a14d58d7e164a6
Huzaifa Qureshi <163686246+huzaifahq63@users.noreply.github.com>
2024-03-18 02:39:45 +0500

    My First Commit

---
+++ Personal Information/main.js
@@ -0,0 +1,8 @@
+var userName = "Huzaifa ";
+console.log("Hello ".concat(userName, ", Qureshi"));
+var age = 20;
+console.log("".concat(age, ", 16 Octuber 2002"));
+var giaiStudent = true;
+console.log("".concat(giaiStudent, ", Governor Initiative For GenAI, Web 3.0, and Metaverse"));
+var Gmail = "huzaifa.hq63@gmai.com";
+console.log(Gmail);
---
+++ Personal Information/main.ts
@@ -0,0 +1,8 @@
+const userName: String = "Huzaifa ";
+console.log(`Hello ${userName}, Qureshi`);
+let age: Number = 20;
+console.log(`${age}, 16 Octuber 2002`);
+let giaiStudent: Boolean = true;
+console.log(`${giaiStudent}, Governor Initiative For GenAI, Web 3.0, and Metaverse`);
+let Gmail: any = "huzaifa.hq63@gmai.com";
+console.log(Gmail);
